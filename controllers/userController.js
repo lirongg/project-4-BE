7046a77a355af9fd8d7b06a05a86b872f6627c2b
okaycan <nglirongg@gmail.com>
2024-05-17 22:10:10 +0800

    add signup signin code

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,2 +1,40 @@
 // Controller methods for user authentication and management
 // Sign in, sign out, delete user, edit user
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
+
+async function create(req, res) {
+  try {
+    const newUserDetails = req.body;
+    const newUser = await User.create(newUserDetails);
+    return res.status(201).json(newUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error.message });
+  }
+}
+
+async function signIn(req, res) {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) throw new Error();
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (!match) throw new Error();
+    res.json(createJWT(user));
+  } catch {
+    res.status(400).json("Bad Credentials");
+  }
+}
+
+function createJWT(user) {
+    return jwt.sign(
+        {user},
+        process.env.SECRET,
+        {expiresIn: "24h"}
+    );
+}
+
+module.exports = {
+    create,signIn
+}
--- server.js
+++ server.js
@@ -1,24 +1,35 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('./config/database')
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
+app.use(require('./config/checkToken'))
+
 //Routes
+app.use('/users', require('./routes/userRoutes'));
 
 
-// Connect to MongoDB
+//defining port and listen
+
+app.listen(PORT, function() {
+    console.log(`Express app is running on port ${PORT}`);
+})
+
 mongoose.connect(process.env.DATABASE_URL);
 
 const db = mongoose.connection;
 
-db.on('connected', function(){
-    console.log(`Connected to ${db.name} at ${db.host}:${db.port}`)
+db.on("connected", function () {
+  console.log(`Connected to ${db.name} at ${db.host}:${db.port}`);
 });
+
+
 
