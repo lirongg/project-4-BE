64a4c20268d8265a7077e6fb5a6d6910d6363705
okaycan <nglirongg@gmail.com>
2024-06-17 14:42:28 +0800

    add location in controller

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const itemCtrl = require("../controllers/itemController");
 
 // all the post, get, put delete routes [router.pose("/create", itemCtrl.createitem)]
+
 router.get("/search", itemCtrl.searchItems)
+router.get('/locations', itemCtrl.getAllLocations);
 router.post("/create", itemCtrl.createItem);
 router.get("/", itemCtrl.getItems)
 router.put('/update/:id', itemCtrl.updateItem);
@@ -12,6 +14,7 @@ router.get('/location/:location',itemCtrl.getitemlocation);
 router.get("/:id", itemCtrl.getUserItems);
 router.get("/item/:id", itemCtrl.getitemId);
 router.put('/item/relocate', itemCtrl.updateItemLocation);
+
 
 
 module.exports = router;
